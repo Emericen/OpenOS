8e45a4edd0b02520df83a20f4163e34f7eb420b1
Eddy <liang.741@buckeyemail.osu.edu>
2025-03-01 13:51:27 -0800

    controller

--- openos/guest.py
+++ openos/guest.py
@@ -21,7 +21,6 @@ class GuestService:
     """
 
     def __init__(self, video_port=8765, control_port=8766):
-
         self.host_ip = None
         self.video_port = video_port
         self.control_port = control_port
@@ -33,11 +32,56 @@ class GuestService:
         self.keyboard_controller = keyboard.Controller()
         self.mouse_controller = mouse.Controller()
 
-        self._get_screen_resolution()
-        # Socket for receiving input commands
-        # self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.resolution = _get_screen_resolution()
 
     def start(self):
+        self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.control_socket.bind(("0.0.0.0", self.control_port))
+
+    def listen_for_input(self):
+        data, addr = self.control_socket.recvfrom(1024)
+        if self.host_ip and addr[0] != self.host_ip:
+            return
+
+        message = json.loads(data.decode())
+        if message["type"] == "setup":
+            self.host_ip = addr[0]
+            print(f"Streaming to host at {self.host_ip}.")
+            self._start_stream()
+        elif message["type"] == "move_mouse":
+            dx, dy = message["data"]["dx"], message["data"]["dy"]
+            self.mouse_controller.move(dx, dy)
+        elif message["type"] == "position_mouse":
+            x, y = message["data"]["x"], message["data"]["y"]
+            self.mouse_controller.position = (x, y)
+        elif message["type"] == "button_down":
+            button_str = _get_button_from_str(message["data"]["button"])
+            self.mouse_controller.press(button_str)
+        elif message["type"] == "button_up":
+            button_str = _get_button_from_str(message["data"]["button"])
+            self.mouse_controller.release(button_str)
+        elif message["type"] == "scroll":
+            dx, dy = message["data"]["dx"], message["data"]["dy"]
+            self.mouse_controller.scroll(dx, dy)
+        elif message["type"] == "key_down":
+            key_str = _get_key_from_str(message["data"]["key"])
+            self.keyboard_controller.press(key_str)
+        elif message["type"] == "key_up":
+            key_str = _get_key_from_str(message["data"]["key"])
+            self.keyboard_controller.release(key_str)
+        else:
+            print(f"[{addr}]: {message['data']}")
+
+    def stop(self):
+        """Stop streaming and close the socket."""
+        if self.control_socket:
+            self.control_socket.close()
+            self.control_socket = None
+        if self.ffmpeg_process:
+            self.ffmpeg_process.terminate()
+            self.ffmpeg_process = None
+
+    def _start_stream(self):
         # fmt: off
         cmd = [
             "ffmpeg", "-f", "x11grab", 
@@ -53,72 +97,36 @@ class GuestService:
         # fmt: on
         self.ffmpeg_process = subprocess.Popen(cmd)
 
-        self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.control_socket.bind(("0.0.0.0", self.control_port))
 
-    def listen_for_input(self):
-        data, addr = self.control_socket.recvfrom(1024)
-        message = json.loads(data.decode())
+def _get_screen_resolution():
+    """Get the current screen resolution using xrandr"""
+    try:
+        output = subprocess.check_output(["xrandr"]).decode("utf-8")
+        # Find the current resolution from the output
+        for line in output.split("\n"):
+            if "*" in line:  # Current resolution has an asterisk
+                resolution = line.split()[0]
+                width, height = map(int, resolution.split("x"))
+                return (width, height)
+    except:
+        # Fallback to default if detection fails
+        return (1920, 1080)
+    
+def _get_button_from_str(button_str):
+    """Get the button from the string"""
+    if button_str == "mouse.Button.left":
+        return mouse.Button.left
+    elif button_str == "mouse.Button.right":
+        return mouse.Button.right
+    elif button_str == "mouse.Button.middle":
+        return mouse.Button.middle
+    return button_str
 
-        if message["type"] == "debug":
-            print(f"[{addr}]: {message['data']}")
-
-    def stop(self):
-        """Stop streaming and close the socket."""
-        if self.control_socket:
-            self.control_socket.close()
-            self.control_socket = None
-
-        if self.ffmpeg_process:
-            self.ffmpeg_process.terminate()
-            self.ffmpeg_process = None
-
-    def _handle_input(self):
-        """Process incoming input commands."""
-        while True:
-            data, addr = self.control_socket.recvfrom(1024)
-            message = json.loads(data.decode())
-
-            # Set client IP if not already set
-            if not self.host_ip:
-                self.host_ip = addr[0]
-                info_message = json.dumps(
-                    {
-                        "type": "system_info",
-                        "data": {
-                            "resolution": self.resolution,
-                        },
-                    }
-                )
-                self.control_socket.sendto(info_message.encode(), addr)
-                # self._start_stream()
-
-            # Process input commands
-            if message["type"] == "keydown":
-                self.keyboard_controller.press(message["data"])
-            elif message["type"] == "keyup":
-                self.keyboard_controller.release(message["data"])
-            elif message["type"] == "mousemove":
-                self.mouse_controller.position = message["data"]
-            elif message["type"] == "mousedown":
-                self.mouse_controller.press(message["data"])
-            elif message["type"] == "mouseup":
-                self.mouse_controller.release(message["data"])
-
-    def _get_screen_resolution(self):
-        """Get the current screen resolution using xrandr"""
-        try:
-            output = subprocess.check_output(["xrandr"]).decode("utf-8")
-            # Find the current resolution from the output
-            for line in output.split("\n"):
-                if "*" in line:  # Current resolution has an asterisk
-                    resolution = line.split()[0]
-                    width, height = map(int, resolution.split("x"))
-                    self.resolution = (width, height)
-        except:
-            # Fallback to default if detection fails
-            self.resolution = (1920, 1080)
-
+def _get_key_from_str(key_str):
+    if key_str.startswith("Key."):
+        key_name = key_str.split(".")[1]
+        return getattr(keyboard.Key, key_name)
+    return key_str
 
 # Example usage when running inside VM
 if __name__ == "__main__":
